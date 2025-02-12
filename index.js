8d9a80e682237f2290a183a0266bcdc7450d65dd
Kinji <linhqwertyuiop2gmail.com>
2025-02-12 19:43:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(
+  cors({ credentials: true, origin: true, exposedHeaders: ["set-cookies"] })
+);
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 6969;
+const MONGO_URL = process.env.MONGO_URL;
+
+//connect database
+mongoose
+  .connect(MONGO_URL)
+  .then(console.log("connect to Mongoose"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log("connect to port " + PORT);
+});
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
